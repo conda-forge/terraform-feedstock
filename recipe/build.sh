f33aa39f8644b55526d5742d5209efd8af8beb92
Patrick Sodré <psodre@gmail.com>
2018-12-21 06:57:03 +0000

    Use compiler and git sources
    @conda-forge-admin, please rerender

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,25 +1,7 @@
 #!/usr/bin/env bash
 
 # Turn the work-folder into GOPATH
-pkg_dir=$(pwd)
-cd ..
-export GOPATH=$(pwd)
-export GOSRC_PREFIX=$GOPATH/src/github.com/hashicorp
-export PATH=$GOPATH/bin:$PATH
-mkdir -p $GOSRC_PREFIX
-
-mv $pkg_dir $GOSRC_PREFIX/$PKG_NAME
-ln -s $GOSRC_PREFIX/$PKG_NAME $pkg_dir
-cd $GOSRC_PREFIX/$PKG_NAME
-
-# Git Initialize
-# Apps tend to use git info to create version string
-git config --global user.email "conda@conda-forge.github.io"
-git config --global user.name "conda-forge"
-git init
-git add conda_build.sh
-git commit -m "conda build of $PKG_NAME-v$PKG_VERSION"
-git tag v${PKG_VERSION}
+pushd src/github.com/hashicorp/$PKG_NAME
 
 # Build
 make fmt
@@ -29,4 +11,4 @@ make dev
 
 # Install Binary into PREFIX/bin
 mkdir -p $PREFIX/bin
-mv $GOPATH/bin/$PKG_NAME $PREFIX/bin/${PKG_NAME}
+mv $GOPATH/bin/$PKG_NAME $PREFIX/bin/$PKG_NAME
